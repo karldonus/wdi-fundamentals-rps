91c01be191a101e1cf3d1e41d3f6a8551cf3d04d
Karl Sakas <donusdonut@Karls-MBP.home>
2015-06-16 15:34:29 -0400

    karl completed rps game

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -1,55 +1,76 @@
-////////////////////////////////////////////////
-/*   Provided Code - Please Don't Edit   */
-////////////////////////////////////////////////
-'use strict';
-
 function getInput() {
     console.log("Please choose either 'rock', 'paper', or 'scissors'.")
-    return prompt();
-}
+    return prompt();}
+    
 function randomPlay() {
     var randomNumber = Math.random();
     if (randomNumber < 0.33) {
-        return "rock";
-    } else if (randomNumber < 0.66) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
-}
-////////////////////////////////////////////////
-/*           Write Your Code Below            */
-////////////////////////////////////////////////
+        return "rock";} 
+     else if (randomNumber < 0.66) {
+        return "paper";} 
+     else {
+        return "scissors";}}
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
-}
+    return move || getInput();}
 
 function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
-}
+     return move || randomPlay();}
 
 function getWinner(playerMove,computerMove) {
     var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    return winner;
-}
+    if (playerMove === computerMove) {
+        return 'tie';}
+    else if (playerMove === 'rock') {
+        if(computerMove === 'scissors') {
+            return 'player';}
+        else {
+            return 'computer';}}
+    else if (playerMove === 'paper') {
+        if(computerMove === 'rock') {
+            return 'player';}
+        else {
+            return 'computer';}}
+    else if (playerMove === 'scissors') {
+        if(computerMove === 'paper') {
+            return 'player';}
+        else {
+            return 'computer';}}}
 
+
+            
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
-}
+    
+function calcAdvantage() {
+    if (playerWins > computerWins) {
+        return 'humanity';}
+    else if (computerWins > playerWins) {
+        return 'skyNet';}
+    else { 
+        return 'nobody'}}
+        
+    while (playerWins < 5 && computerWins < 5) {
+        var computerMove = getComputerMove();
+        var playerMove = getPlayerMove();
+        var winner = getWinner(playerMove,computerMove);
+         
+        if (winner === 'player') {
+            playerWins += 1;}
+        else if (winner === 'computer') {
+            computerWins += 1;}
+        else {console.log('Doah! It is a tie, try again.')}
+    
+        var advantage = calcAdvantage();
+    
+    console.log('You cast a ' + playerMove + ' while skyNet chose ' + computerMove);
+    console.log('The score is now ' + playerWins + ' to ' + computerWins + ' in favor of ' + advantage);}
+    
+    console.log('Oh, looks like ' + advantage + ' won... good job');
+ 
+    return [playerWins, computerWins];}
+
+playToFive();
 
